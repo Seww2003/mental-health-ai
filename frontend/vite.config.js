20de8b625dedf07b9e5bac1986c70cf1bd0d0242
Seww2003 <sewwandisenavirathna887@gmail.com>
2026-06-18 22:11:42 +0530

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-base: "./",
- 
+  base: '/mental-health-ai/',   // 🔥 GitHub repo name  match 
 
   plugins: [react()],
+
 
   server: {
     port: 3000,
